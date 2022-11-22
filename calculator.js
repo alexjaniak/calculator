e7dcb7b4f37dc3308ffeb3fd3e31a109a03142b7
alexjaniak <alexander.janiak.acc@outlook.com>
2022-11-22 14:25:25 +0100

    format calculator css

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
-leftInput = null; 
-rightInput = null; 
-currOperator = null; 
-isResult = false;
+const MAXINPUT = 10;
+let leftInput = null; 
+let rightInput = null; 
+let currOperator = null; 
+let isResult = false;
 
 // operators
 const add = (a, b) => a+b;
@@ -13,6 +14,8 @@ function operate(a, b, operation) {
     return operation(a, b);
 }
 
+
+// nodes
 input = document.querySelector('#input');
 input.textContent = '';
 
@@ -22,20 +25,62 @@ clear = document.querySelector('#clear');
 equal = document.querySelector('#equal');
 percent = document.querySelector('#perc'); 
 
+
+function matchOperator(id) {
+    switch (id) {
+        case 'add':
+            return add;
+        case 'sub':
+            return sub;
+        case 'mult':
+            return mult;
+        case 'div':
+            return div; 
+    }
+}
+
+function roundValue(val) {
+    if(val.toString().length <= MAXINPUT) return val;
+}
+
+function evaluate() {
+    console.log(currOperator,leftInput, rightInput);
+    if (leftInput == null || currOperator == null) return; 
+    if (leftInput != null && rightInput == null) rightInput = leftInput; 
+    output = operate(leftInput, rightInput, currOperator);
+    input.textContent = output.toString();
+    console.log(output);
+    leftInput = output; 
+    rightInput = null;
+    currOperator = null; 
+    isResult = true;
+}
+
+
 // append listeners
 digits.forEach(digit => {
     digit.addEventListener('click', () => {
         console.log(currOperator,leftInput, rightInput);
+        if (input.textContent.length > MAXINPUT) { // check for maximum length
+            return;
+        } 
+
         if (currOperator == null) {
-            if (isResult) {
+            if (isResult) { // clear input box if input after result
                 input.textContent = '';
                 isResult = false;
+            }
+            if (digit.textContent == '.') { // check for double decimals
+                if (input.textContent.includes('.')) return; 
             }
             input.textContent += digit.textContent;
             leftInput = parseFloat(input.textContent)
         } else {
-            if (rightInput == null) {
+            if (rightInput == null) { // clear input box if second second argument
                 input.textContent = '';
+            }
+            if (digit.textContent == '.') { // check for double decimals
+                if (input.textContent.includes('.')) return; 
             }
             input.textContent += digit.textContent;
             rightInput = parseFloat(input.textContent);
@@ -51,19 +96,6 @@ clear.addEventListener('click', () => {
     currOperator = null; 
 })
 
-function evaluate() {
-    console.log(currOperator,leftInput, rightInput);
-    if (leftInput == null || currOperator == null) return; 
-    if (leftInput != null && rightInput == null) rightInput = leftInput; 
-    output = operate(leftInput, rightInput, currOperator);
-    input.textContent = output.toString();
-    console.log(output);
-    leftInput = output; 
-    rightInput = null;
-    currOperator = null; 
-    isResult = true;
-}
-
 equal.addEventListener('click', evaluate);
 
 percent.addEventListener('click', () =>{
@@ -74,23 +106,15 @@ percent.addEventListener('click', () =>{
     else rightInput = newValue;
 })
 
-function matchOperator(id) {
-    switch (id) {
-        case 'add':
-            return add;
-        case 'sub':
-            return sub;
-        case 'mult':
-            return mult;
-        case 'div':
-            return div; 
-    }
-}
-
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
         if (leftInput == null) return; 
-        currOperator = matchOperator(operator.id)
+        newOperator = matchOperator(operator.id)
+        if (newOperator == currOperator) {
+            evaluate();
+            return;
+        } else currOperator = newOperator;
+
         if (rightInput != null) evaluate();
     });
 })
